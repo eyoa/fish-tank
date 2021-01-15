091c92fae7bbe3a76b29d880a9c2203bbe31e4b9
Evelyn Yoa <yoaevelyn@gmail.com>
2021-01-15 22:07:29 +0000

    added new type of fish bite fish and updated the sprites

--- public/scripts/entities/switch_fish.js
+++ public/scripts/entities/switch_fish.js
@@ -1,4 +1,9 @@
 class SwitchFish extends Fish {
+  constructor(options){
+    super(options);
+    this.imageUri = './images/spikeFish.png'
+
+  }
 
   onClick(event) {
     this.makeNewVelocity(50);
