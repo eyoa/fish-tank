02ebd84b96127302ed7b6e48862daf105e110d38
Evelyn Yoa <yoaevelyn@gmail.com>
2021-01-15 22:50:58 +0000

    added eat ability to bitefish

--- public/scripts/entities/bite_fish.js
+++ public/scripts/entities/bite_fish.js
@@ -4,4 +4,28 @@ class BiteFish extends Fish{
     this.imageUri = './images/bitefish.gif';
 
   }
+
+  updateOneTick(){
+    super.updateOneTick();
+    let radar = this.tank.getProximateDenizens(this.position, 50);
+    if(radar.length > 0){
+      this.eat(radar);
+    }
+  }
+
+  eat(radar){
+    for (let prey of radar){
+      // Not yourself and doesn't eat starter or seeds
+      if (prey !== this){
+        if(prey instanceof Starter){
+          return false;
+        }else if (prey instanceof Seed){
+          return false;
+        }else {
+          prey.kill();
+        }
+      }
+
+    }
+  }
 }
